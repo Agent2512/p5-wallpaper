171def7ca13cbe795e28a0890b7ee25f6344ec21
agent2512 <spiler2512@live.dk>
2021-05-23 17:37:55 +0200

    23-05-2021

--- sketch.js
+++ sketch.js
@@ -1,151 +1,126 @@
 var agents = [];
-var trails = [];
-var moveSpeed = 4
-var r_angle = 15
-var ellipse_size = 10
+var n_agents = 180
+var n_line = 5
+var ellipse_size = 8
+var line_size = 1
+var startBorder = 50
+var speed = .6
+var backSpeed = 1
 
-setInterval(() => {
-    // moveSpeed = moveSpeed == 1 ? 4 : 1
-}, 1000 * 3);
+var ele = document.getElementById('container')
 
 var sketch = function (p) {
     p.setup = () => {
-        p.createCanvas(800, 800, p.P2D);
+        p.createCanvas(ele.offsetWidth, ele.offsetHeight, p.P2D);
         p.pixelDensity(1);
         p.background(0)
-
-        for (var i = 0; i < 50; i++) {
-            agents.push(new agnet2(p.width / 2, p.height / 2, p.random(360)))
-            // agents.push(new agnet2(p.random(p.width), p.random(p.height), p.random(360)))
-        }
-    };
-
-    p.draw = () => {
-        // p.background(0)
-
-
+        p.stroke(255)
         p.ellipseMode(p.CENTER);
         p.angleMode(p.DEGREES);
         p.colorMode(p.RGB)
 
-        test()
-        fade()
-    }
+        for (var i = 0; i < n_agents; i++) {
+            // agents.push(new agnet(p.width / 2, p.height / 2, i, p))
+            agents.push(new agnet(p.random(startBorder, p.width - startBorder), p.random(startBorder, p.height - startBorder), i, p))
+        }
+    };
 
-    fade = () => {
+    p.draw = () => {
+        p.background(0)
 
-        for (let i = 0; i < trails.length; i++) {
-            const pixel_v = trails[i];
-            const pixel = p.get(pixel_v.x, pixel_v.y)
-
-            var color_removeI = 2
-            let c = p.color(pixel[0] - color_removeI, pixel[1] - color_removeI, pixel[2] - color_removeI);
-            p.stroke(c)
-            p.fill(c)
-            p.ellipse(pixel_v.x, pixel_v.y, ellipse_size)
-
-            var color_limit = 20
-            if (pixel[0] <= color_limit && pixel[1] <= color_limit && pixel[2] <= color_limit) {
-                let c = p.color(0);
-                p.stroke(c)
-                p.fill(c)
-                p.ellipse(pixel_v.x, pixel_v.y, ellipse_size + 1)
-
-                trails.splice(i, 1)
-            }
-
-            p.updatePixels();
+        for (a of agents) {
+            a.update()
+            a.show()
         }
     }
-    update = () => {
-        for (let a of agents) {
-            var cos = p.cos(a.angle)
-            var sin = p.sin(a.angle)
-            var dir = p.createVector(cos, sin)
-            var pos = p.createVector(a.pos.x, a.pos.y)
-            var newPos = pos.add(dir).mult(moveSpeed)
 
-            if (newPos.x < 0 || newPos.x >= p.width || newPos.y < 0 || newPos.y >= p.height) {
-                newPos.x = p.min(p.width - 0.01, p.max(0, newPos.x))
-                newPos.y = p.min(p.height - 0.01, p.max(0, newPos.y))
-
-                a.angle = a.angle + p.random(100, 260)
-            }
-
-            a.angle = a.angle + p.random(-r_angle, r_angle)
-
-            a.angle >= 360 && (a.angle = a.angle % 360)
-
-            a.pos = newPos
-
-            if (trails.find(i => i.id == `${p.floor(newPos.x)}${p.floor(newPos.y)}`) == undefined) {
-                trails.push({ x: p.floor(newPos.x), y: p.floor(newPos.y), id: `${p.floor(newPos.x)}${p.floor(newPos.y)}` })
-            }
-
-            // a.pos = { x: p.floor(newPos.x), y: p.floor(newPos.y) }
-        }
+    p.windowResized = () => {
+        p.resizeCanvas(ele.offsetWidth, ele.offsetHeight);
     }
-    show = () => {
-        for (let a of agents) {
-            let c = p.color(255);
-            p.set(a.pos.x, a.pos.y, c)
-            p.updatePixels();
-        }
-    }
-    var color_state;
 
-
-    test = () => {
-        for (let a of agents) {
-            // pos
-            var cos = p.cos(a.angle)
-            var sin = p.sin(a.angle)
-            var dir = p.createVector(cos, sin).mult(moveSpeed)
-            var pos = p.createVector(a.pos.x, a.pos.y)
-            var newPos = pos.add(dir)
-
-
-            if (newPos.x < 0 || newPos.x >= p.width || newPos.y < 0 || newPos.y >= p.height) {
-                newPos.x = p.min(p.width - 0.01, p.max(0, newPos.x))
-                newPos.y = p.min(p.height - 0.01, p.max(0, newPos.y))
-
-                a.angle = a.angle + p.random(100, 260)
-
-                // color_state = [
-                //     p.random(20, 200), 
-                //     p.random(20, 200), 
-                //     p.random(20, 200)
-                // ]
-            }
-
-            a.angle = a.angle + p.random(-r_angle, r_angle)
-
-            a.angle >= 360 && (a.angle = a.angle % 360)
-
-            a.pos = newPos
-
-            var id = `${newPos.x}${newPos.y}`
-            if (trails.find(i => i.id == id) == undefined) {
-                trails.push({ x: newPos.x, y: newPos.y, id })
-            }
-
-            // show
-            let c = p.color(255);
-            p.stroke(c)
-            p.fill(c)
-            p.ellipse(a.pos.x, a.pos.y, ellipse_size)
-            p.updatePixels();
-        }
-    }
 };
 
 
 
 new p5(sketch, "container");
 
-class agnet2 {
-    constructor(x, y, angle) {
-        this.angle = angle
-        this.pos = { x, y }
+class agnet {
+    constructor(x, y, id, p5) {
+        this.id = id;
+        this.p5 = p5;
+        this.pos = this.p5.createVector(x, y);
+
+        this.move = [0, 0]
+
+        setInterval(() => {
+            var rx = this.p5.round(this.p5.random(0, 1))
+            var ry = this.p5.round(this.p5.random(0, 1))
+
+            if (rx == 0) this.move[0] = -speed;
+            else if (rx == 1) this.move[0] = speed;
+
+
+            if (ry == 0) this.move[1] = -speed;
+            else if (ry == 1) this.move[1] = speed;
+        }, 1000);
+    }
+
+    update() {
+        var x = this.pos.x, y = this.pos.y;
+
+        var [moveX, moveY] = this.move
+
+        x += moveX
+        y += moveY
+
+        const newPos = this.p5.createVector(x, y)
+
+
+        this.check(newPos)
+        this.pos.set(newPos)
+
+
+    }
+
+    show() {
+        this.p5.strokeWeight(1)
+        this.p5.ellipse(this.pos.x, this.pos.y, ellipse_size)
+        this.nearby()
+    }
+
+
+    check(pos) {
+        if (pos.x <= 0 || pos.x >= this.p5.width || pos.y <= 0 || pos.y >= this.p5.height) {
+            // agents[this.id] = new agnet(this.p5.random(startBorder,this.p5.width-startBorder), this.p5.random(startBorder,this.p5.height-startBorder), this.id, this.p5)
+            // agents[this.id] = new agnet(this.pos.x, this.pos.y, this.id, this.p5)
+        }
+
+
+        if (pos.x <= 0) {
+            this.move[0] = backSpeed
+        }
+        if (pos.x >= this.p5.width) {
+            this.move[0] = -backSpeed
+
+        }
+        if (pos.y <= 0) {
+            this.move[1] = backSpeed
+        }
+        if (pos.y >= this.p5.height) {
+            this.move[1] = -backSpeed
+        }
+
+    }
+
+
+    nearby() {
+        var f_agents = agents.filter(i => i.id != this.id)
+        var s_agents = f_agents.sort((a, b) => a.pos.dist(this.pos) - b.pos.dist(this.pos))
+        s_agents.length = n_line
+
+        for (a of s_agents) {
+            this.p5.strokeWeight(line_size)
+            this.p5.line(this.pos.x, this.pos.y, a.pos.x, a.pos.y)
+        }
     }
 }
